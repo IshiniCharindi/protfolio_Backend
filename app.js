b83eadae76976994e6826d33397d8a8ea0a188ef
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2025-11-09 14:51:00 +0530

    update live urls

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ app.get('/', (req, res) => {
     res.send('Backend is running!');
 });
 
+app.get('/api/health', (req, res) => {
+    res.status(200).json({ status: 'ok' });
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
